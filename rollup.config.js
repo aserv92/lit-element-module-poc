04817dcaf4fe928313b2eb07cf2a6e80efcc363f
Andrew Servi <andrew@analytehealth.com>
2023-11-02 14:18:31 -0500

    Create rollup config

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import { litScss } from 'rollup-plugin-scss-lit'
+import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import summary from 'rollup-plugin-summary';
+
+const _exports = [
+  './src/stdcheck-header/StdcheckHeader.ts',
+  './src/stdcheck-header/stdcheck-header.ts',
+];
+
+const getExports = () => {
+  return _exports.map((_export) => (
+    {
+      input: _export,
+      output: [{
+        file: _export.replace('./src', './dist').replace('.ts', '.js'),
+        format: 'iife',
+        sourcemap: true,
+        globals: {
+          'lit': 'Lit',
+        }
+      }],
+      watch: {
+        skipWrite: false,
+        clearScreen: false,
+        include: 'src/**/*',
+        exclude: 'node_modules/**',
+        chokidar: {
+            paths: 'src/**/*',
+            usePolling: false
+        }
+      },
+      plugins: [
+        litScss({
+          minify: true,
+          options: { loadPaths: ['node_modules'] }
+        }),
+        resolve(),
+        terser({
+          ecma: 2021,
+          module: true,
+          warnings: true,
+        }),
+        summary(),
+      ]
+    }
+  ));
+}
+
+export default getExports();
