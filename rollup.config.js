10e38fd61c3d29dca8e95b5d7f7df35849eb2fb6
Andrew Servi <andrew@analytehealth.com>
2023-11-03 15:55:07 -0500

    Generate both minified and un-minified JS

--- rollup.config.js
+++ rollup.config.js
@@ -8,40 +8,49 @@ const bundles = [
   './src/healthlabs-bundle.ts',
 ];
 
+const getConfig = (fileName, minified) => {
+  return {
+    input: fileName,
+    output: [{
+      file: fileName.replace('./src', './dist').replace('.ts', `${minified ? '.min' : ''}.js`),
+      format: 'iife',
+      sourcemap: minified,
+      globals: {
+        'lit': 'Lit',
+      }
+    }],
+    watch: {
+      skipWrite: false,
+      clearScreen: false,
+      include: 'src/**/*',
+      exclude: 'node_modules/**',
+      chokidar: {
+        paths: 'src/**/*',
+        usePolling: true
+      }
+    },
+    plugins: [
+      litScss({
+        minify: minified,
+        options: {loadPaths: ['node_modules']}
+      }),
+      typescript(),
+      resolve(),
+      summary(),
+    ],
+    preserveEntrySignatures: 'strict',
+  };
+}
+
 const getExports = () => {
-  return bundles.map((bundle) => (
-    {
-      input: bundle,
-      output: [{
-        file: bundle.replace('./src', './dist').replace('.ts', '.js'),
-        format: 'esm',
-        sourcemap: true,
-        globals: {
-          'lit': 'Lit',
-        }
-      }],
-      watch: {
-        skipWrite: false,
-        clearScreen: false,
-        include: 'src/**/*',
-        exclude: 'node_modules/**',
-        chokidar: {
-            paths: 'src/**/*',
-            usePolling: true
-        }
-      },
-      plugins: [
-        litScss({
-          minify: true,
-          options: { loadPaths: ['node_modules'] }
-        }),
-        typescript(),
-        resolve(),
-        summary(),
-      ],
-      preserveEntrySignatures: 'strict',
-    }
-  ));
+  let exports = [];
+
+  bundles.forEach((bundleFileName) => {
+    exports.push(getConfig(bundleFileName, true));
+    exports.push(getConfig(bundleFileName, false));
+  });
+
+  return exports;
 }
 
 export default getExports();
