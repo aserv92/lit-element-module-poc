e8faeca828a8f928bb5e749483444a625b38b594
Andrew Servi <andrew@analytehealth.com>
2023-11-03 14:37:14 -0500

    Make POC for Lit working with SCSS and bulma

--- src/stdcheck-footer/stdcheck-footer.ts
+++ src/stdcheck-footer/stdcheck-footer.ts
@@ -1,12 +1,59 @@
 import { html, LitElement } from 'lit';
 import StdcheckFooterStyle from './stdcheck-footer.scss';
+import { query } from 'lit/decorators.js';
 
 export class StdcheckFooter extends LitElement {
+  @query('div#dropdown') dropdown: HTMLDivElement;
+
   static styles = [StdcheckFooterStyle];
+
+  handleMouseover() {
+    this.dropdown.classList.add('is-active');
+  }
+
+  handleMouseout() {
+    this.dropdown.classList.remove('is-active');
+  }
 
   render() {
     return html`
       <h2>STDCheck Footer</h2>
+     <div
+       id="dropdown"
+       @mouseover="${this.handleMouseover}"
+       @mouseout="${this.handleMouseout}"
+       class="dropdown"
+     >
+      <div id="dropdown" class="dropdown">
+        <div class="dropdown-trigger">
+          <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+            <span>Dropdown button</span>
+            <span class="icon is-small">
+              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+          </button>
+        </div>
+        <div class="dropdown-menu" id="dropdown-menu" role="menu">
+          <div class="dropdown-content">
+            <a href="#" class="dropdown-item">
+              Dropdown item
+            </a>
+            <a class="dropdown-item">
+              Other dropdown item
+            </a>
+            <a href="#" class="dropdown-item is-active">
+              Active dropdown item
+            </a>
+            <a href="#" class="dropdown-item">
+              Other dropdown item
+            </a>
+            <hr class="dropdown-divider">
+            <a href="#" class="dropdown-item">
+              With a divider
+            </a>
+          </div>
+        </div>
+      </div>
     `;
   }
 }
