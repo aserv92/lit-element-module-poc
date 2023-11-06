fd24518744bbd346247c4e8935e6b95b8e39dcb6
Andrew Servi <andrew@analytehealth.com>
2023-11-06 14:36:10 -0600

    Simplify Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import summary from 'rollup-plugin-summary';
 import typescript from "@rollup/plugin-typescript";
 
-const bundles = [
-  './src/stdcheck-bundle.ts',
-  './src/healthlabs-bundle.ts',
-];
-
 const getConfig = (fileName, minified) => {
   return {
     input: fileName,
@@ -42,15 +37,9 @@ const getConfig = (fileName, minified) => {
   };
 }
 
-const getExports = () => {
-  let exports = [];
-
-  bundles.forEach((bundleFileName) => {
-    exports.push(getConfig(bundleFileName, true));
-    exports.push(getConfig(bundleFileName, false));
-  });
-
-  return exports;
-}
-
-export default getExports();
+export default [
+  getConfig('./src/stdcheck-bundle.ts', true),
+  getConfig('./src/stdcheck-bundle.ts', false),
+  getConfig('./src/healthlabs-bundle.ts', true),
+  getConfig('./src/healthlabs-bundle.ts', false),
+];
